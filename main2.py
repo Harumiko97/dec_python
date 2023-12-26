755a8552d75694a91c16e8cdd8c108a36b580abb
Harumiko97 <marharyta.polishchuk@gmail.com>
2023-12-26 12:35:09 +0200

    Update

--- main2.py
+++ main2.py
@@ -1,91 +1,72 @@
-import operator
-from functools import wraps
-import time
-from functools import lru_cache
-
 # Task 1
+class Discount:
+    """
+    Default discount.
+    """
+
+    def __init__(self, discount=0):
+        self.discount = 0
+        if discount > 0:
+            self.discount = discount
 
 
-# Task 2
-def log_info(information):
-    @wraps(information)
-    def wrapped(*args, **kwargs):
-        print('log init value:', args[0])
-        res = information(*args, **kwargs)
-        print('log result:', res)
-        return res
-    return wrapped
+class RegularDiscount(Discount):
+    """
+    Regular discount for a customer.
+    """
+
+    def __init__(self, reg_discount, x):
+        super().__init__(discount=0)
+        self.reg_discount = x - x * 0.10
+
+    def __str__(self):
+        return f'{self.reg_discount}'
 
 
-@log_info
-def function(a):
-    return a + 10
+class SilverDiscount(Discount):
+    """
+    Silver discount for a customer.
+    """
+
+    def __init__(self, sil_discount, x):
+        super().__init__(discount=0)
+        self.sil_discount = x - x * 0.30
+
+    def __str__(self):
+        return f'{self.sil_discount}'
 
 
-print('Result:', function(10))
+class GoldDiscount(Discount):
+    """
+    Gold discount for a customer.
+    """
 
-# Task 3
+    def __init__(self, gold_discount, x):
+        super().__init__(discount=0)
+        self.gold_discount = x - x * 0.50
 
-def timeit(func):
-    @wraps(func)
-    def timeit_wrapper(*args, **kwargs):
-        start_time = time.perf_counter()
-        res = func(*args, **kwargs)
-        end_time = time.perf_counter()
-        total_time = end_time - start_time
-        print(f'Function {func.__name__}{args}{kwargs} Took {total_time}')
-        return res
-    return timeit_wrapper
+    def __str__(self):
+        return f'{self.gold_discount}'
 
-@timeit
-def math(num):
-    total = sum((num for i in range(0, num ** 2)))
-    return total
+
+class Client:
+    """
+    Client's info and discount.
+    """
+
+    def __init__(self, name, discount, order):
+        self.name = name
+        self.discount = discount
+        self.order = order
+
+    def get_total_price(self, discount: int | float = 1, order: int | float = 1):
+        return order * discount
+
+    def __str__(self):
+        return f'{self.name}, {self.discount}, {self.get_total_price}'
+
 
 if __name__ == '__main__':
-    math(10)
-    math(20)
-    math(30)
-
-
-# Task 4
-def rate_limits(max_calls, period):
-    def decorator(func):
-        calls = 0
-        last_reset = time.time()
-
-        def wrapper(*args, **kwargs):
-            nonlocal calls, last_reset
-            elapsed = time.time() - last_reset
-            if elapsed > period:
-                calls = 0
-                last_reset = time.time()
-            if calls >= max_calls:
-                raise Exception('Rate limit exceeded.')
-            calls += 1
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
-
-
-@rate_limits(max_calls=3, period=5)
-def api_call():
-    print('API call success.')
-    for _ in range(8):
-        try:
-            api_call()
-        except Exception as e:
-            print(f'Error: {e}')
-            return api_call
-
-
-# Task 5
-@lru_cache(maxsize=None)
-def fib(n):
-    if n < 2:
-        return n
-    return fib(n-1) + fib(n-2)
-
-
-print(([fib(n) for n in range(16)]))
-print(fib.cache_info())
+    main_order = Client('Mary', 0.3, 100)
+    main_order.get_total_price()
+    print(main_order)
