eea5a73e40bbfb3b131efaf22db0e7fb6ed14436
Harumiko97 <marharyta.polishchuk@gmail.com>
2024-01-02 18:09:16 +0200

    Update

--- main2.py
+++ main2.py
@@ -1,72 +1,114 @@
-# Task 1
-class Discount:
-    """
-    Default discount.
-    """
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-    def __init__(self, discount=0):
-        self.discount = 0
-        if discount > 0:
-            self.discount = discount
+terminal_handler = logging.StreamHandler()
+file_handler = logging.FileHandler('log.txt')
+terminal_handler.setLevel(logging.DEBUG)
+file_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+terminal_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(terminal_handler)
+logger.addHandler(file_handler)
+
+
+class Discount:
+    def discount(self):
+        raise NotImplementedError
+
+
+class DiscountLimit(Exception):
+    def __init__(self, limit_discount, message='Invalid discount'):
+        self.limit_discount = limit_discount
+        self.message = message
+        super().__init__()
+
+    def __str__(self):
+        return f'Limit of discount is {self.limit_discount}\n. {self.message}'
 
 
 class RegularDiscount(Discount):
-    """
-    Regular discount for a customer.
-    """
-
-    def __init__(self, reg_discount, x):
-        super().__init__(discount=0)
-        self.reg_discount = x - x * 0.10
-
-    def __str__(self):
-        return f'{self.reg_discount}'
+    def discount(self):
+        return 0.7
 
 
 class SilverDiscount(Discount):
-    """
-    Silver discount for a customer.
-    """
-
-    def __init__(self, sil_discount, x):
-        super().__init__(discount=0)
-        self.sil_discount = x - x * 0.30
-
-    def __str__(self):
-        return f'{self.sil_discount}'
+    def discount(self):
+        return 0.05
 
 
 class GoldDiscount(Discount):
-    """
-    Gold discount for a customer.
-    """
+    def discount(self):
+        return 0.1
 
-    def __init__(self, gold_discount, x):
-        super().__init__(discount=0)
-        self.gold_discount = x - x * 0.50
+
+class PlatinumDiscount(Discount):
+    def discount(self):
+        return 0.15
+
+
+class Product:
+    def __init__(self, name: str, price: float):
+        self.name = name
+        self.price = price
 
     def __str__(self):
-        return f'{self.gold_discount}'
+        return f'{self.name} - {self.price}'
 
 
-class Client:
-    """
-    Client's info and discount.
-    """
-
-    def __init__(self, name, discount, order):
+class Customer:
+    def __init__(self, name: str, discount: Discount):
         self.name = name
         self.discount = discount
-        self.order = order
 
-    def get_total_price(self, discount: int | float = 1, order: int | float = 1):
-        return order * discount
+
+class Order:
+    def __init__(self):
+        self.products = []
+        self.quantities = []
+
+    def add_product(self, product: Product, quantity: int):
+        self.products.append(product)
+        self.quantities.append(quantity)
+
+    def total_price(self, customer: Customer = None):
+        discount = customer.discount.discount() if customer else 0
+        total = 0
+        for product, quantity in zip(self.products, self.quantities):
+            total += product.price * quantity
+        return total * (1 - discount)
 
     def __str__(self):
-        return f'{self.name}, {self.discount}, {self.get_total_price}'
+        return '\n'.join(map(str, self.products))
 
+
+discounts = {
+    'regular': RegularDiscount(),
+    'silver': SilverDiscount(),
+    'gold': GoldDiscount(),
+    'platinum': PlatinumDiscount(),
+}
 
 if __name__ == '__main__':
-    main_order = Client('Mary', 0.3, 100)
-    main_order.get_total_price()
-    print(main_order)
+    print('Welcome to our shop!')
+    print('Available discounts:')
+    for discount in discounts:
+        try:
+            discounts
+        except DiscountLimit:
+            raise DiscountLimit(0.4)
+    for discount in discounts:
+        print(discount)
+    discount = input('Please choose discount: ')
+    customer = Customer('John', discounts[discount])
+
+    product1 = Product('banana', 100)
+    product2 = Product('apple', 200)
+    order = Order()
+    order.add_product(product1, 2)
+    order.add_product(product2, 1)
+
+    print(order.total_price(customer))
